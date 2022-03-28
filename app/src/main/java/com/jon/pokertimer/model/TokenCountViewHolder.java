6b2d11add8b4642764eeac7276f788d03d1c3350
Jonathan DURAND <djjd50@hotmail.fr>
2022-03-28 22:39:12 +0200

    Fix counter Token, just need detect on change value

--- app/src/main/java/com/jon/pokertimer/model/TokenCountViewHolder.java
+++ app/src/main/java/com/jon/pokertimer/model/TokenCountViewHolder.java
@@ -2,6 +2,8 @@ package com.jon.pokertimer.model;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,6 +12,13 @@ import com.jon.pokertimer.R;
 
 public class TokenCountViewHolder extends RecyclerView.ViewHolder {
 
+    Token token;
+
+    View colorSquare;
+    TextView colorText;
+    TextView valueText;
+    EditText countText;
+
     TokenCountAdapter.OnTokenCountListener onTokenCountListener;
     public TokenCountViewHolder(@NonNull View itemView, TokenCountAdapter.OnTokenCountListener onTokenCountListener) {
         super(itemView);
@@ -17,13 +26,29 @@ public class TokenCountViewHolder extends RecyclerView.ViewHolder {
 
     private void createAllButtons() {
         Button addOne = itemView.findViewById(R.id.buttonAdd1);
-        addOne.setOnClickListener(view -> {});
+        addOne.setOnClickListener(view -> token.addNumber(1));
         Button addFive = itemView.findViewById(R.id.buttonAdd5);
-        addFive.setOnClickListener(view -> {});
+        addFive.setOnClickListener(view -> token.addNumber(5));
         Button addTen = itemView.findViewById(R.id.buttonAdd10);
-        addTen.setOnClickListener(view -> {});
+        addTen.setOnClickListener(view -> token.addNumber(10));
+        Button resetButton = itemView.findViewById(R.id.buttonResetToken);
+        resetButton.setOnClickListener(view -> token.setNumber(0));
+    }
+
+    private void setAllText() {
+        colorSquare = this.itemView.findViewById(R.id.colorTokenView2);
+        colorText = this.itemView.findViewById(R.id.colorToken2);
+        valueText = this.itemView.findViewById(R.id.tokenCountVal);
+        countText = this.itemView.findViewById(R.id.tokenCountNumber);
     }
 
     public void display(Token token) {
+        this.token = token;
+        createAllButtons();
+        setAllText();
+        colorSquare.setBackgroundColor(token.getColor().getColorValue());
+        colorText.setText(token.getColor().getName());
+        valueText.setText(token.getValueToString());
+        countText.setText(token.getNumberToString());
     }
 }
